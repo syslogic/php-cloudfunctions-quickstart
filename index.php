162343f715b9144f5427938dac5c8881a8e2a816
Martin Zeitler <martin@syslogic.io>
2022-02-19 15:05:30 +0100

    checking if vendor/autoload.php is readable.

--- index.php
+++ index.php
@@ -1,9 +1,16 @@
 <?php
+// Check Auto-Loader
+if ( !is_dir(__DIR__.'/vendor') || !is_readable(__DIR__.'/vendor/autoload.php')) {
+    die('vendor/autoload.php could not be read');
+}
+
+// Apply Auto-Loader
 require_once __DIR__.'/vendor/autoload.php';
 use Google\CloudFunctions\FunctionsFramework;
 
-// Register the function with Functions Framework.
+// Register functions with the Functions Framework.
 // https://console.cloud.google.com/functions/list
-FunctionsFramework::http(        "on_https", "Quickstart\\CloudFunctions::on_https");
-FunctionsFramework::cloudEvent( "on_pubsub", "Quickstart\\CloudFunctions::on_pubsub");
-FunctionsFramework::cloudEvent(    "on_gcs", "Quickstart\\CloudFunctions::on_gcs");
+FunctionsFramework::http(        'on_https', 'Quickstart\\CloudFunctions::on_https');
+FunctionsFramework::cloudEvent( 'on_pubsub', 'Quickstart\\CloudFunctions::on_pubsub');
+FunctionsFramework::cloudEvent(    'on_gcs', 'Quickstart\\CloudFunctions::on_gcs');
+
